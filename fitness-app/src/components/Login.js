25c4fc9194e04c34b7f2174b99633afeb4e4d799
jacksonarrey <agborarrey93@gmail.com>
2023-09-26 05:08:36 +0100

    Fitness-class

---
+++ fitness-app/src/components/Login.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "../styles/login.css";
+
+const Login = () => {
+  const [action, setAction] = useState("sign up");
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">{action}</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        {action === "login" ? (
+          <div></div>
+        ) : (
+          <div className="input">
+            <input type="text" placeholder="Name" />
+          </div>
+        )}
+
+        <div className="input">
+          <input type="email" name="email" placeholder="Email" />
+        </div>
+        <div className="input">
+          <input type="password" name="password" placeholder="password" />
+        </div>
+        {action === "sign up" ? (
+          <div></div>
+        ) : (
+          <div className="forgot-password">
+            lost password ? <span>click Here</span>
+          </div>
+        )}
+
+        <div className="submit container">
+          <div
+            className={action === "login" ? "submit gray" : "submit"}
+            onClick={() => {
+              setAction("sign up");
+            }}
+          >
+            sign up
+          </div>
+          <div
+            className={action === "sign up" ? "submit gray" : "submit"}
+            onClick={() => {
+              setAction("login");
+            }}
+          >
+            login
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
